3c0acec7b59846f08a4b5d4b5683b1e4c0ceae40
DrGamePlayzz <akmalyapar12@gmail.com>
2026-02-20 09:17:56 +0700

    some fix

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -86,6 +86,8 @@ module.exports = {
 	
         let slogs = time + ` (${interaction.user.id})${interaction.member.displayName}: /${interaction.commandName}.\n`;
 
-        fs.appendFileSync(`/sdcard/Dyru/MortalEmployee/logs/${year}-${month}-${date}.txt`, String(slogs));
+		const p = `/sdcard/Dyru/MortalEmployee/logs/${year}-${month}-${date}.txt`;
+        if (!fs.existSync(p)) return fs.writeFileSync(p, String(slogs));
+        fs.appendFileSync(p), String(slogs);
     }
 };
